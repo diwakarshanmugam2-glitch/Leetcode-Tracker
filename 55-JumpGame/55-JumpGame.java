eb297f4635392cc167235578cb301d7ee4207293
diwakarshanmugam2-glitch <diwakarshanmugam2@gmail.com>
2026-07-14 14:02:47 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,15 @@
+// Last updated: 7/14/2026, 2:02:47 PM
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+        
+        return true;
+    }
+}
