14afd5e0e191973dbb4c060e76d83c2c19266d01
diwakarshanmugam2-glitch <diwakarshanmugam2@gmail.com>
2026-07-14 14:02:08 +0530

    Create 95-UniqueBinarySearchTreesIi

---
+++ 95-UniqueBinarySearchTreesIi/95-UniqueBinarySearchTreesIi.java
@@ -0,0 +1,30 @@
+// Last updated: 7/14/2026, 2:02:09 PM
+
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) return new ArrayList<>();
+        return buildTrees(1, n);
+    }
+
+    private List<TreeNode> buildTrees(int start, int end) {
+        List<TreeNode> result = new ArrayList<>();
+        if (start > end) {
+            result.add(null);
+            return result;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftTrees = buildTrees(start, i - 1);
+            List<TreeNode> rightTrees = buildTrees(i + 1, end);
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    result.add(root);
+                }
+            }
+        }
+
+        return result;
+    }
+}
