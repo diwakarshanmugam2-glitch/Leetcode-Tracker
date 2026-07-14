c425f7c9edb93cfdb9e0149391c29e591f6ed1bd
diwakarshanmugam2-glitch <diwakarshanmugam2@gmail.com>
2026-07-14 14:03:39 +0530

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.java
@@ -0,0 +1,39 @@
+// Last updated: 7/14/2026, 2:03:39 PM
+import java.util.*;
+
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int sum = 0;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            int currentVal = map.get(s.charAt(i));
+            if (i < n - 1 && currentVal < map.get(s.charAt(i + 1))) {
+                sum -= currentVal;
+            } else {
+                sum += currentVal;
+            }
+        }
+
+        return sum;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.romanToInt("III"));      
+        System.out.println(sol.romanToInt("LVIII"));    
+        System.out.println(sol.romanToInt("MCMXCIV"));  
+    }
+}
+       
+       
+    
+      
