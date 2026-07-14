5873bb8bd5ddd2d04baa1feda9e569b3859c143b
diwakarshanmugam2-glitch <diwakarshanmugam2@gmail.com>
2026-07-14 14:02:55 +0530

    Create 46-Permutations

---
+++ 46-Permutations/46-Permutations.py
@@ -0,0 +1,18 @@
+# Last updated: 7/14/2026, 2:02:55 PM
+class Solution:
+    def permute(self, nums):
+        res = []
+
+        def backtrack(path, remaining):
+            if not remaining:
+                res.append(path)
+                return
+
+            for i in range(len(remaining)):
+                backtrack(
+                    path + [remaining[i]],
+                    remaining[:i] + remaining[i+1:]
+                )
+
+        backtrack([], nums)
+        return res
