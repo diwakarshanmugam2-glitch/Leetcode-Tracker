65b0eeabe93463f054b910d65c6cf7ceb02e34a4
diwakarshanmugam2-glitch <diwakarshanmugam2@gmail.com>
2026-07-14 14:02:12 +0530

    Create 93-RestoreIpAddresses

---
+++ 93-RestoreIpAddresses/93-RestoreIpAddresses.py
@@ -0,0 +1,24 @@
+# Last updated: 7/14/2026, 2:02:12 PM
+class Solution:
+    def restoreIpAddresses(self, s):
+        res = []
+
+        def backtrack(start, path):
+            if len(path) == 4:
+                if start == len(s):
+                    res.append(".".join(path))
+                return
+
+            for i in range(1, 4):
+                if start + i > len(s):
+                    break
+
+                part = s[start:start + i]
+
+                if (part[0] == '0' and len(part) > 1) or int(part) > 255:
+                    continue
+
+                backtrack(start + i, path + [part])
+
+        backtrack(0, [])
+        return res
