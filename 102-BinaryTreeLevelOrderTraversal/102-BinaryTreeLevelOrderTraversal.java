2b5af680eb6c25ecf1902dc6c430cf4110853006
diwakarshanmugam2-glitch <diwakarshanmugam2@gmail.com>
2026-07-14 14:02:04 +0530

    Create 102-BinaryTreeLevelOrderTraversal

---
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,22 @@
+// Last updated: 7/14/2026, 2:02:05 PM
+import java.util.*;
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);       
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode current = queue.poll();
+                level.add(current.val);
+                if (current.left != null) queue.offer(current.left);
+                if (current.right != null) queue.offer(current.right);
+            }
+            result.add(level);
+        }        
+        return result;
+    }
+}
