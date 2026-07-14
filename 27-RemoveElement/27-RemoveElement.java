f139961d40ffc2394e01d09280ab4c050672fd61
diwakarshanmugam2-glitch <diwakarshanmugam2@gmail.com>
2026-07-14 14:03:23 +0530

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.java
@@ -0,0 +1,16 @@
+// Last updated: 7/14/2026, 2:03:23 PM
+class Solution {
+    public int removeElement(int[] nums, int val) {
+
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
