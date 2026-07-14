6664ed9f07c716e51acb683789583f1bca30e71a
diwakarshanmugam2-glitch <diwakarshanmugam2@gmail.com>
2026-07-14 14:00:41 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.java
@@ -0,0 +1,14 @@
+// Last updated: 7/14/2026, 2:00:42 PM
+class Solution {
+public int fib(int n) {
+        int a=0;
+        int b=1;
+        for(int i =0;i<n;i++){
+            int c= a+b;
+            a=b;
+            b=c;
+        }
+        return a;
+        
+    }
+}
