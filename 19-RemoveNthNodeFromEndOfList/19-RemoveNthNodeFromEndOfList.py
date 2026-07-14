d412c860d9ca2c781b5c9c2e118d25fc950b48a6
diwakarshanmugam2-glitch <diwakarshanmugam2@gmail.com>
2026-07-14 14:03:30 +0530

    Create 19-RemoveNthNodeFromEndOfList

---
+++ 19-RemoveNthNodeFromEndOfList/19-RemoveNthNodeFromEndOfList.py
@@ -0,0 +1,18 @@
+# Last updated: 7/14/2026, 2:03:30 PM
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        dummy = ListNode(0)
+        dummy.next = head
+        first = dummy
+        second = dummy
+
+        for _ in range(n + 1):
+            first = first.next
+
+        while first is not None:
+            first = first.next
+            second = second.next
+
+        second.next = second.next.next
+
+        return dummy.next
